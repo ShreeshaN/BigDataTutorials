7c9943f2ec5d99877800c8877298223ed45d0009
ShreeshaN <nshreesha2011@gmail.com>
2019-10-02 20:13:23 -0400

    created 5 splits for json input format

--- src/com/project2/custominputformat/JSONInputFormat.java
+++ src/com/project2/custominputformat/JSONInputFormat.java
@@ -11,6 +11,13 @@ import java.io.IOException;
 
 public class JSONInputFormat extends FileInputFormat<Text, Text> {
 
+
+    @Override
+    protected long computeSplitSize(long blockSize, long minSize, long maxSize) {
+        // the size of airfield.json is 967618 bytes. So to create 5 splits we need to divide the file by 200000
+        return 200000;
+    }
+
     @Override
     public RecordReader<Text, Text> createRecordReader(InputSplit inputSplit, TaskAttemptContext taskAttemptContext) throws IOException, InterruptedException {
         JSONRecordReader jsonRecordReader = new JSONRecordReader();
